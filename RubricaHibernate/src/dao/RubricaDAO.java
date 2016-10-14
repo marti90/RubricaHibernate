2a2f9881a2d7ab48b3996f8e65a327b158fe3ad0
marti90 <debernardi.martina@gmail.com>
2016-10-14 14:20:30 +0200

    secondo e terzo metodo DAOrubrica

--- RubricaHibernate/src/dao/RubricaDAO.java
+++ RubricaHibernate/src/dao/RubricaDAO.java
@@ -17,19 +17,45 @@ import model.RubricaModel;
 
 public class RubricaDAO {
 	
-	public RubricaModel readRubrica(String nome){
+	public RubricaModel readRubricaConId(long r_id){
 		
-        RubricaModel r = null;
+		RubricaModel r = null;
 		Session session=HibernateUtility.openSession();
 		Transaction tx=null;
-		String sql="FROM RUBRICA WHERE NOME=: 1";
-		Query query = session.createQuery(sql);
 		
 		try{
 	        tx=session.getTransaction();
 	        tx.begin();
 	        
-	        query.setString(1, nome);
+	        r=session.get(RubricaModel.class,  r_id);
+	        
+	        tx.commit(); 
+            
+	    }catch(Exception ex){
+	         tx.rollback();
+
+	    }finally{
+	         session.close();
+	    }
+	    
+		return r;
+		
+	}
+		
+	public RubricaModel readRubricaConNome(String nome){
+		
+        RubricaModel r = null;
+		Session session=HibernateUtility.openSession();
+		Transaction tx=null;
+	
+		try{
+	        tx=session.getTransaction();
+	        tx.begin();
+	        
+	        String sql="FROM RUBRICA WHERE NOME=: nomeInserito";
+			Query query = session.createQuery(sql);
+	        
+	        query.setString("nomeInserito", nome);
 	        r = (RubricaModel) query.uniqueResult();
 	        
 	        tx.commit(); 
@@ -44,10 +70,10 @@ public class RubricaDAO {
 		return r;
 	}
 	
-	public void creaRubrica(RubricaModel r){
+	public boolean createRubrica(RubricaModel r){
 	
+		boolean res = false;
 		Session session=HibernateUtility.openSession();
-		
 		Transaction tx=null;
 		
 		try{
@@ -57,6 +83,7 @@ public class RubricaDAO {
 		       session.persist(r);
 		        
 		       tx.commit(); 
+		       res = true;
 	            
 	     }catch(Exception ex){
 		       tx.rollback();
@@ -64,68 +91,33 @@ public class RubricaDAO {
 	     }finally{
 		       session.close();
 	     }
+		
+		return res;
 	}
 	
-    public RubricaModel leggiRubricaConId(long id){
-		
-		RubricaModel r = null;
-		
-		Connection con = null;
-        String sql = "SELECT * FROM RUBRICA WHERE ID_RUBRICA=?";
-		ResultSet res = null;
-		PreparedStatement pst = null;
-		
-		try {
-			con = DataSource.getInstance().getConnection();
-			pst= con.prepareStatement(sql);
-			pst.setLong(1, id);
-			res= pst.executeQuery();
-			
-			if(res.next()){
-				long id_rubrica= res.getLong(1);
-				String nome = res.getString(2);
-				
-				r= new RubricaModel(id_rubrica,nome);
-			}
-		
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (IOException | PropertyVetoException e1) {
-			e1.printStackTrace();
-		
-		}finally{
-			if (res != null) try { res.close(); } catch (SQLException e) {e.printStackTrace();}
-			if (pst != null) try { pst.close(); } catch (SQLException e) {e.printStackTrace();}
-			if (con != null) try { con.close(); } catch (SQLException e) {e.printStackTrace();}
-		}
-		
-		return r;
-	}
-    
-    public boolean eliminaRubrica(long id_rubrica){
+    public boolean deleteRubrica(long id_rubrica){
     	
     	boolean res = false;
-    	Connection con = null;
-    	String sql = "DELETE FROM RUBRICA WHERE ID_RUBRICA=?";
-    	PreparedStatement pst = null;
-    	
-    	try {
-			 con = DataSource.getInstance().getConnection();
-			 pst = con.prepareStatement(sql);
-			 pst.setLong(1, id_rubrica);
-			 int rs =pst.executeUpdate();
-			 if(rs>0){
-				 res=true;
-			 }
-			 
-		} catch (SQLException | IOException | PropertyVetoException e) {
-			e.printStackTrace();
-		} finally{
-			//if (res != null) try { res.close(); } catch (SQLException e) {e.printStackTrace();}
-			if (pst != null) try { pst.close(); } catch (SQLException e) {e.printStackTrace();}
-			if (con != null) try { con.close(); } catch (SQLException e) {e.printStackTrace();}
-		}
-    	return res;
+		Session session=HibernateUtility.openSession();
+		Transaction tx=null;
+		
+		try{
+		       tx=session.getTransaction();
+		       tx.begin();
+		        
+		       session.persist(r);
+		        
+		       tx.commit(); 
+		       res = true;
+	            
+	     }catch(Exception ex){
+		       tx.rollback();
+
+	     }finally{
+		       session.close();
+	     }
+		
+		return res;
     }
 
 }
